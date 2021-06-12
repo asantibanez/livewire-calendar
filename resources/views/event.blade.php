446a464d836d673495903f6d27c0c4a38aa19f88
USS-EXCELSIOR\Steve <steve@stevyrey.co.uk>
2021-06-12 01:18:19 +0100

    An implementation for events that span multiple days. This uses a bool called multiday on the event. If the event is not a multi day event, event.blade.php now shows the start and end times.

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -7,6 +7,11 @@
     <p class="text-sm font-medium">
         {{ $event['title'] }}
     </p>
+    <p class="mt-0 text-xs">
+        @if(!isset($event['multiday']) || $event['multiday']!== true)
+            {{$event['date']->format('G:i')}} - {{$event['end']->format('G:i')}}
+        @endif
+    </p>
     <p class="mt-2 text-xs">
         {{ $event['description'] ?? 'No description' }}
     </p>
