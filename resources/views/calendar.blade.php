cb4684d08602a7efdc5b6b99e7599d173f4b6806
Usman Manzoor <147910873+usman19900@users.noreply.github.com>
2024-01-23 16:22:31 +0500

    Update calendar.blade.php

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,39 +1,39 @@
-<div
-    @if($pollMillis !== null && $pollAction !== null)
-        wire:poll.{{ $pollMillis }}ms="{{ $pollAction }}"
+<div class="p-6 bg-gray-200 border border-gray-500 rounded-lg shadow dark:bg-gray-800"
+    @if ($pollMillis !== null && $pollAction !== null) wire:poll.{{ $pollMillis }}ms="{{ $pollAction }}"
     @elseif($pollMillis !== null)
-        wire:poll.{{ $pollMillis }}ms
-    @endif
->
+        wire:poll.{{ $pollMillis }}ms @endif>
+    <div class="flex justify-between items-center mb-4">
+        <button wire:click="goToPreviousMonth" class="bg-brown py-1.5 px-3 rounded-md text-white" style="background-color: blue">Previous Month</button>
+        <div class="font-bold text-2xl">
+            <span>{{ $startsAt->format('F Y') }}</span>
+        </div>
+        <button wire:click="goToNextMonth" class="bg-brown py-1.5 px-3 rounded-md text-white" style="background-color: blue">Next Month</button>
+    </div>
     <div>
         @includeIf($beforeCalendarView)
     </div>
 
-    <div class="flex">
-        <div class="overflow-x-auto w-full">
-            <div class="inline-block min-w-full overflow-hidden">
+    <div class="">
 
-                <div class="w-full flex flex-row">
-                    @foreach($monthGrid->first() as $day)
-                        @include($dayOfWeekView, ['day' => $day])
-                    @endforeach
-                </div>
+        <div class="w-full flex flex-row rounded-lg " style="background-color: blue; ">
+            @foreach ($monthGrid->first() as $day)
+                @include($dayOfWeekView, ['day' => $day])
+            @endforeach
+        </div>
 
-                @foreach($monthGrid as $week)
-                    <div class="w-full flex flex-row">
-                        @foreach($week as $day)
-                            @include($dayView, [
-                                    'componentId' => $componentId,
-                                    'day' => $day,
-                                    'dayInMonth' => $day->isSameMonth($startsAt),
-                                    'isToday' => $day->isToday(),
-                                    'events' => $getEventsForDay($day, $events),
-                                ])
-                        @endforeach
-                    </div>
+        @foreach ($monthGrid as $week)
+            <div class="w-full flex flex-row ">
+                @foreach ($week as $day)
+                    @include($dayView, [
+                        'componentId' => $componentId,
+                        'day' => $day,
+                        'dayInMonth' => $day->isSameMonth($startsAt),
+                        'isToday' => $day->isToday(),
+                        'events' => $getEventsForDay($day, $events),
+                    ])
                 @endforeach
             </div>
-        </div>
+        @endforeach
     </div>
 
     <div>
