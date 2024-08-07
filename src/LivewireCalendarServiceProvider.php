02a59f7d0608a49eb29b210455f4e3751eb06d43
Caique Bispo <caiquebispo0023@gmail.com>
2024-08-07 20:18:19 -0300

    update information packge composer.json

--- src/LivewireCalendarServiceProvider.php
+++ src/LivewireCalendarServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider;
 
 class LivewireCalendarServiceProvider extends ServiceProvider
 {
-    /**
+    /**git 
      * Bootstrap the application services.
      */
     public function boot()
