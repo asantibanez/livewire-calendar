832414e7a375d31bcace2a9a3347b09fa0810bbd
asantibanez <santibanez.andres@gmail.com>
2025-03-14 17:46:54 -0500

    Fixed calculation

--- src/LivewireCalendar.php
+++ src/LivewireCalendar.php
@@ -175,8 +175,8 @@ class LivewireCalendar extends Component
         $firstDayOfGrid = $this->gridStartsAt;
         $lastDayOfGrid = $this->gridEndsAt;
 
-        $numbersOfWeeks = $lastDayOfGrid->diffInWeeks($firstDayOfGrid) + 1;
-        $days = $lastDayOfGrid->diffInDays($firstDayOfGrid) + 1;
+        $numbersOfWeeks = ceil($lastDayOfGrid->diffInWeeks($firstDayOfGrid, true));
+        $days = ceil($lastDayOfGrid->diffInDays($firstDayOfGrid, true));
 
         if ($days % 7 != 0) {
             throw new Exception("Livewire Calendar not correctly configured. Check initial inputs.");
